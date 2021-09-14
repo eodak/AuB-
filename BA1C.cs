90b53f7c6e71c8055126f588e1c54c1a8b51a151
eodak <89468059+eodak@users.noreply.github.com>
2021-09-14 17:12:11 +0200

    Add files via upload

---
+++ BA1C.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BA1C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "CGTTAC";
+            string sk = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                if (text.Substring(i, 1) == "A")
+                {
+                    sk += "T";
+                }
+                else if (text.Substring(i, 1) == "T")
+                {
+                    sk += "A";
+                }
+                else if (text.Substring(i, 1) == "G")
+                {
+                    sk += "C";
+                }
+                else
+                {
+                    sk += "G";
+                }
+            }
+            Console.WriteLine(sk);
+        }
+    }
+}
